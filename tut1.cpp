e94e490d090bbb499e910adf584636a743db7103
Divyansh5002 <beastboydiv2005@gmail.com>
2026-07-13 10:51:39 +0530

    MY FIRST C++ PROGRAM

---
+++ tut1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a = 20;
+    int b = 9;
+
+    cout << "the value of a " << a
+         << " the value of b " << b;
+
+        return 0;
