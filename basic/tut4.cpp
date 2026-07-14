438fd18a088043a0578039fa6766821059f21c00
Divyansh5002 <beastboydiv2005@gmail.com>
2026-07-14 15:52:42 +0530

    tut4.cpp

---
+++ basic/tut4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a =3;
+    int*b =&a;
+    cout<<"the adress of b is"<<b;   
+    cout<<"the value of b is"<<*b;   
+       
+return 0;
+}
