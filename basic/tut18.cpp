edcff76aa375bdbe13a846a35bdd519153bce114
Divyansh5002 <beastboydiv2005@gmail.com>
2026-07-17 17:31:50 +0530

    mergi

--- basic/tut18.cpp
+++ basic/tut18.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main(){
-    string str;
-    string str1="";
-    cout<<"enter the string"<<endl;
-    cin>>str;
+    int n,n1;
+    cout<<"enter the no of elements u want in your 2 arrays" <<endl;
+    cin>>n;
+    cin>>n1;
+    int arr[n],arr1[n1];
+    for (int i=0;i<n;i++){
+        cout<<"enter the value of arr"<<i<<endl;
+        cin>>arr[i];
+    }
+    for (int i=0;i<n1;i++){
+        cout<<"enter the value of arr1"<<i<<endl;
+        cin>>arr1[i];
+    }
     int leng;
-    leng=str.length();
-    for (int i=0;i<leng;i++){
-        str1=str[i]+str1;
-
+    leng=n+n1;
+    int arr2[leng];
+    for(int i=0;i<leng;i++){
+        if(i<n){
+            arr2[i]=arr[i];
+        }
+        else{
+            arr2[i]=arr1[i-n];
+        }
     }
-    if(str1==str){
-        cout<<"the string is palindrome"<<endl;
-    }
-    else{
-        cout<<"the string is not palindrome"<<endl;
-    }return 0;
-
+    cout<<"the merged array is"<<endl;
+    for(int i=0;i<leng;i++){
+        cout<<"the value of arr2"<<i<<"is"<<arr2[i]<<endl;
+    }   
+    return 0;
 }
